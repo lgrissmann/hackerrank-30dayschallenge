ab3e18308775d68d8a403b79b03ba7c764b1ee22
Luiz Gustavo Rissmann <lg@rissmann.org>
2020-09-28 10:31:11 -0300

    Day 4: Class vs. Instance - Solved

--- d-04-Call vs Instance.java
+++ d-04-Call vs Instance.java
@@ -23,16 +23,48 @@ public class Person {
     private int age;	
   
 	public Person(int initialAge) {
-  		// Add some more code to run some checks on initialAge
+        // Add some more code to run some checks on initialAge
+        if (initialAge > 0) {
+            this.age = initialAge;
+        } else {
+            this.age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        } 
 	}
 
 	public void amIOld() {
   		// Write code determining if this person's age is old and print the correct statement:
-        System.out.println(/*Insert correct print statement here*/);
+        
+        String output = "You are old.";
+
+        if ( this.age < 13){
+            output = "You are young.";
+        
+        }else if ( this.age >= 13 &&  this.age < 18){
+            output = "You are a teenager.";
+        }
+
+        System.out.println(output);
 	}
 
 	public void yearPasses() {
-  		// Increment this person's age.
+        // Increment this person's age.
+        this.age++;
 	}
 
 	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
