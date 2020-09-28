c5ee8f921349c2ec550c9946c52237eca38b12e1
Luiz Gustavo <lg@rissmann.com.br>
2020-09-28 09:54:09 -0300

    Dia 00- Hello World

---
+++ d-00-Hello World.sh
@@ -0,0 +1,8 @@
+# get a line of input from stdin and save it to our variable
+read inputString 
+
+# Your first line of output goes here
+echo 'Hello, World.'
+
+# Write the second line of output
+echo $inputString
