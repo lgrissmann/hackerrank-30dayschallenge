04902152d269cb3fe94504d9d0c3acef937eb698
Luiz Gustavo <lg@rissmann.com.br>
2020-09-28 09:54:33 -0300

    Day 02 - Operators

---
+++ d-02-Operators.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        
+        double add_tip = new Double(tip_percent) / 100 * meal_cost;
+        double add_tax = new Double(tax_percent) / 100 * meal_cost;
+        
+        double total = meal_cost + add_tip + add_tax;
+
+        System.out.println(Math.round(total));
+        ;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
